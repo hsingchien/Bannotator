ca1695fa2a28f71cbf440a73f614c7a9907703fe
rustynerve <hsingchien.zhang@gmail.com>
2023-05-02 00:05:23 -0700

    data test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 from bannotator.data import Annotation
+import numpy as np
 
 TEST_ANNOTATION_FILE_LARGE = os.path.join("tests", "annotation", "annotation.txt")
 TEST_ANNOTATION_FILE_SMALL = os.path.join("tests", "annotation", "annotation_small.txt")
@@ -36,7 +37,8 @@ def test_annotation_load_from_txt(large_annotation, small_annotation, config_fil
     assert len(config_file.get_streams()) == 2
     assert len(small_annotation.get_streams()) == 3
     assert large_annotation.get_stream(1).ID == 1
-    assert large_annotation._validate_stream()
+    for _,stream in large_annotation.get_streams().items():
+        assert verify_epochs(stream.get_epochs(), True)
     with pytest.raises(Exception):
         annot = Annotation({})
         annot.read_from_file(txt_path=TEST_ANNOTATION_FILE_ERROR)
@@ -70,9 +72,20 @@ def test_add_behavior(small_annotation):
         assert new_key[new_behav.index("jump")] == "q"
 
 def test_delete_behavior(large_annotation):
-    original_behav = [b.name for b in small_annotation.get_behaviors()[0]]
+    original_behav = [b.name for b in large_annotation.get_behaviors()[0]]
     assert "dig" in original_behav
-    
+    # Get dig epoch list
+    vect_before_rmv = np.ones((large_annotation.get_length(),large_annotation.num_stream()))
+    i = 0
+    for _, stream in large_annotation.get_streams().items():
+        epochs = stream.get_behavior_dict()["dig"].get_epochs()
+        digID = stream.get_behavior_dict()["dig"].ID
+        otherID = stream.get_behavior_dict()["other"].ID
+        vect_before_rmv[:,i] = stream.get_stream_vect()
+        i+=1
+        for epoch in epochs:
+            assert epoch.name == "dig"
+            assert epoch.get_behavior() is stream.get_behavior_dict()["dig"]
     large_annotation.delete_behavior("dig", "other")
     behaviors = large_annotation.get_behaviors()
     # Test if the target behavior is removed
@@ -81,5 +94,52 @@ def test_delete_behavior(large_annotation):
         assert "dig" not in new_behav
         assert len(original_behav) - len(new_behav) == 1
     # Verify the epochs of dig are merged into others
+    vect_after_rmv = np.ones((large_annotation.get_length(),large_annotation.num_stream()))
+    i = 0
+    for _,stream in large_annotation.get_streams().items():
+        other = stream.get_behavior_dict()["other"]
+        # Test if there is no faulty epochs in other
+        assert verify_epochs(other.get_epochs(), False)
+        assert verify_epochs(stream.get_epochs(), True)
+        vect_after_rmv[:,i] = stream.get_stream_vect()
+        i+=1
+    # Test if indices of dig OR other before removal is the same with indices of other after remvoval
+    for i in range(large_annotation.num_stream()):
+        before_rmv = vect_before_rmv[:,i]
+        after_rmv = vect_after_rmv[:,i]
+        bf_idcs = np.argwhere(np.isin(before_rmv, (digID, otherID)))
+        aft_idcs = np.argwhere(after_rmv == otherID)
+        assert np.setdiff1d(bf_idcs, aft_idcs).size == 0 and np.setdiff1d(aft_idcs, bf_idcs).size == 0
+            
+def test_add_stream(small_annotation):
+    new_stream = small_annotation.add_stream("other")
+    assert small_annotation.num_stream() == 4
+    assert len(new_stream.get_epochs()) == 1
+    epoch = new_stream.get_epochs()[0]
+    assert epoch.name == "other"
         
-        
+def verify_epochs(epochs, continuous = False):
+    epochs.sort(reverse=False)
+    if continuous and epochs[0].start != 1:
+        return False
+    for i in range(len(epochs)-1):
+        if epochs[i].end >= epochs[i+1].start:
+            return False
+        if not continuous:
+            # Verify epochs of a Behavior
+            # No adjacent epochs with same behavior
+            if epochs[i+1].start - epochs[i].end == 1:
+                return False
+        if continuous:
+            # Verify epochs of a Stream
+            # Must have no blanks and no adjacent epochs with same Behavior
+            if epochs[i+1].name == epochs[i].name:
+                return False
+            if epochs[i+1].start - epochs[i].end != 1:
+                return False
+    return True
+
+def verify_behaviors(annotation):
+    blist = []
+    for _,stream in annotation.get_streams().items():
+        blist.append([b.name for b in stream.get_behavior_list()])
