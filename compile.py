d68393727cf0b6c968f463501418f940939bb690
XingjianZhang <hsingchien.zhang@gmail.com>
2023-05-14 20:54:25 -0700

    compile script

--- compile.py
+++ compile.py
@@ -1,18 +1,26 @@
 import subprocess
 import os
+import platform
 
 path_to_file = os.path.join(os.getcwd(), "bannotator")
 path_to_app = os.path.join(path_to_file, "app.py")
-
 pc_path_to_icon = os.path.join(path_to_file, "resources", "icon.ico")
 mac_path_to_icon = os.path.join(path_to_file, "resources", "icon.icns")
-pc_command = "pyinstaller -i " + pc_path_to_icon + " " + path_to_app
-# subprocess.run(pc_command)
-
-mac_command = (
-    "pyinstaller --onefile --windowed --icon "
-    + mac_path_to_icon
-    + " --osx-bundle-identifier com.honglab.annotator --name annotator "
-    + path_to_app
-)
-subprocess.run(["pyinstaller", "--onefile", "--windowed","--icon",mac_path_to_icon,"--osx-bundle-identifier","com.honglab.annotator","--name","annotator",path_to_app])
+if platform.system() == "Windows":
+    subprocess.run(["pyinstaller", "--clean", "-i", pc_path_to_icon, path_to_app])
+elif platform.system() == "Darwin":
+    subprocess.run(
+        [
+            "pyinstaller",
+            "--clean",
+            "--onefile",
+            "--windowed",
+            "--icon",
+            mac_path_to_icon,
+            "--osx-bundle-identifier",
+            "com.honglab.annotator",
+            "--name",
+            "annotator",
+            path_to_app,
+        ]
+    )
