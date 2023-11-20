b1fd30502a988b5d0d0d55ffe773469d491d6548
XingjianZhang <hsingchien.zhang@gmail.com>
2023-11-19 21:27:36 -0800

    package release

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-import pathlib
-
-here = pathlib.Path(__file__).parent.resolve()
-
-setup(
-    name="bannotator",
-    version="0.1.0",
-    author="Xingjian Zhang",
-    description="A GUI application to annotator animal behavior videos",
-    packages=find_packages(),
-    project_urls={"Source Code": "https://github.com/hsingchien/Bannotator"},
-    entry_points={"console_scripts": ["annotate-behavior=bannotator.app:main"]},
-    python_requires="==3.9",
-)
+setup()
---
+++ src/bannotator/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+from importlib import resources
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the realpython-reader package
+__version__ = "1.0.2"
