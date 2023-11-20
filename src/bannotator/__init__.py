825b8c5408e4f09b5a3c7bd12efbdd04a542ad07
XingjianZhang <hsingchien.zhang@gmail.com>
2023-11-19 21:49:41 -0800

    bug fix

--- src/bannotator/__init__.py
+++ src/bannotator/__init__.py
@@ -1,10 +1,6 @@
 # __init__.py
 
 from importlib import resources
-try:
-    import tomllib
-except ModuleNotFoundError:
-    import tomli as tomllib
 
 # Version of the realpython-reader package
 __version__ = "1.0.3"
